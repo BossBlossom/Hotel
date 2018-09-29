5d17969258e4269b7678675961d233d680d0d84e
YuuSatou970726 <Yuu.Satou.970726@gmail.com>
2018-09-29 16:59:30 +0900

    hotel

---
+++ Project/hotel/src/main/java/vn/com/java/config/MyApp.java
@@ -0,0 +1,23 @@
+package vn.com.java.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyApp extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { SecurityWebConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {MyWebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+}
