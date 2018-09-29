fb3931c552453efecfc1b69d2db832a01864d74c
YuuSatou970726 <Yuu.Satou.970726@gmail.com>
2018-09-29 17:21:56 +0900

    update

--- Project/hotel/src/main/java/vn/com/java/config/MyWebConfig.java
+++ Project/hotel/src/main/java/vn/com/java/config/MyWebConfig.java
@@ -67,9 +67,9 @@ public class MyWebConfig implements WebMvcConfigurer
 	public DataSource getDataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/shigotonikki");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/hotel");
 		dataSource.setUsername("root");
-		dataSource.setPassword("427Shinigami");
+		dataSource.setPassword("root");
 		return dataSource;
 	}
 
