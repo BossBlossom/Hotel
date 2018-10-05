4b0055515563a0c1a00ab9b5a43722e24049d25e
YuuSatou970726 <Yuu.Satou.970726@gmail.com>
2018-10-05 20:37:58 +0900

    add Model(Room/UploadForm)

---
+++ Project/hotel/src/main/java/vn/com/java/model/RoomModel.java
@@ -0,0 +1,89 @@
+package vn.com.java.model;
+
+import vn.com.java.entity.Room;
+
+public class RoomModel 
+{
+	private int roomNo;
+	
+	private String status;
+	
+	private int bed;
+	
+	private int airConditioner;
+	
+	private int money;
+	
+	private String information;
+	
+	public Room toRoom()
+	{
+		Room room = new Room();
+		room.setRoomNo(this.roomNo);
+		room.setStatus(this.status);
+		room.setBed(this.bed);
+		room.setAirConditioner(this.airConditioner);
+		room.setMoney(this.money);
+		room.setInformation(this.information);
+		return room;
+	}
+	
+	public void fromRoom(Room room)
+	{
+		this.setRoomNo(room.getRoomNo());
+		this.setStatus(room.getStatus());
+		this.setBed(room.getBed());
+		this.setAirConditioner(room.getAirConditioner());
+		this.setMoney(room.getMoney());
+		this.setInformation(room.getInformation());
+	}
+
+	public int getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getBed() {
+		return bed;
+	}
+
+	public void setBed(int bed) {
+		this.bed = bed;
+	}
+
+	public int getAirConditioner() {
+		return airConditioner;
+	}
+
+	public void setAirConditioner(int airConditioner) {
+		this.airConditioner = airConditioner;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public String getInformation() {
+		return information;
+	}
+
+	public void setInformation(String information) {
+		this.information = information;
+	}
+	
+}
