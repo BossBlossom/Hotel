0ba7e4b1ddb7fd5343b16f7d695fe688ea1cbed2
thienan <ndsg1964@gmail.com>
2018-10-09 20:47:22 +0700

    manager-update-room.jsp

--- Project/hotel/src/main/java/vn/com/java/controller/RoomController.java
+++ Project/hotel/src/main/java/vn/com/java/controller/RoomController.java
@@ -52,7 +52,7 @@ public class RoomController
 		return "redirect:/manager-list";
 	}
 	
-	@RequestMapping(value="/update_room", method = RequestMethod.GET)
+	@RequestMapping(value="/manager-update_room", method = RequestMethod.GET)
 	public String Update(@RequestParam(name="roomNo")int roomNo, Model model)
 	{
 		Room room = roomService.find(roomNo);
@@ -69,7 +69,7 @@ public class RoomController
 		return "manager-update-room";
 	}
 	
-	@RequestMapping(value = "/update_room", method = RequestMethod.POST)
+	@RequestMapping(value = "/manager-update_room", method = RequestMethod.POST)
 	public String handleUpdate(@RequestParam(name="roomNo")int roomNo, @ModelAttribute("room") RoomModel roomModel, BindingResult result, Model model)
 	{
 		if(result.hasErrors())
