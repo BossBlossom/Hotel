3f72eddea6c0cdcd5af567838514bce988a40977
Thien An <ndsg1964@gmail.com>
2019-04-06 20:20:16 +0700

    Create View

--- Project/hotel/src/main/java/vn/com/java/config/MyWebConfig.java
+++ Project/hotel/src/main/java/vn/com/java/config/MyWebConfig.java
@@ -71,7 +71,7 @@ public class MyWebConfig implements WebMvcConfigurer
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/hotel");
 		dataSource.setUsername("root");
-		dataSource.setPassword("root");
+		dataSource.setPassword("thienan99");
 		return dataSource;
 	}
 
